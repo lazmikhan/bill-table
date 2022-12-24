800c0bc9c587bf98747fea7b2f2a07997cae69fa
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-12-24 20:15:29 +0800

    added

--- src/components/BillInput/Summary.js
+++ src/components/BillInput/Summary.js
@@ -16,7 +16,7 @@ const Summary = ({arr1, arr2}) => {
       <th className='border border-slate-300' rowSpan={2}>Rate</th>
       <th className='border border-slate-300' colSpan={3}>Quantity</th>
      
-      <th className='border border-slate-300' rowSpan={2}>Amount</th>
+      <th className='border border-slate-300' colSpan={3}>Amount</th>
       <th className='border border-slate-300' rowSpan={2}>Remarks</th>
     </tr>
     <tr>
@@ -24,7 +24,12 @@ const Summary = ({arr1, arr2}) => {
         <th className='border border-slate-300'>This</th>
         <th className='border border-slate-300'>Previous</th>
         <th className='border border-slate-300'>Cummulitive</th>
+        <th className='border border-slate-300'>This</th>
+        <th className='border border-slate-300'>Previous</th>
+        <th className='border border-slate-300'>Cummulitive</th>
+        
     </tr>
+    
     <tr className='text-yellow'>
     <th className='border border-slate-300'>A</th>
     <th className='border border-slate-300 bg-[#1da1f2]'>GRANITE WORK</th>
@@ -35,10 +40,20 @@ const Summary = ({arr1, arr2}) => {
     <th className='border border-slate-300'></th>
     <th className='border border-slate-300'></th>
     <th className='border border-slate-300'></th>
+    <th className='border border-slate-300'></th>
+    <th className='border border-slate-300'></th>
 
  
     </tr>
-{    arr1.map( item=>(<SummaryInput item={item}></SummaryInput>))}
+    
+        
+    {    arr1.map( item=>(<tr>
+        <td className='border border-slate-300'>{item.count1}</td>
+        <td className='border border-slate-300'>{item.description}</td>
+       
+    </tr>))}
+    
+
 <tr className='text-yellow'>
     <th className='border border-slate-300'>B</th>
     <th className='border border-slate-300 bg-[#1da1f2]'>TILES WORK</th>
@@ -49,13 +64,28 @@ const Summary = ({arr1, arr2}) => {
     <th className='border border-slate-300'></th>
     <th className='border border-slate-300'></th>
     <th className='border border-slate-300'></th>
+    <th className='border border-slate-300'></th>
+    <th className=' border border-slate-300'></th>
 
  
     </tr>
  
 {    arr2.map( item=>(<SummaryTiles item={item}></SummaryTiles>))}
 <br />
+<tr>
+
+<th  className='border border-slate-300'></th>
+<th  className='border border-slate-300'></th>
+<th  className='border border-slate-300'></th>
+<th  className='border border-slate-300'></th>
+    <th colSpan={3} className='border border-slate-300'>Total Amount</th>
+    <th  className='border border-slate-300'></th>
+    <th  className='border border-slate-300'></th>
+    <th  className='border border-slate-300'></th>
+    <th  className='border border-slate-300'></th>
+</tr>
 </table>
+<br />
 <Link className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' to="/billInput">Go Back to Input</Link>
         </div>
     );
--- src/components/BillInput/SummaryInput.js
+++ src/components/BillInput/SummaryInput.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 
 const SummaryInput = (props) => {
 const {description}= props.item;
-const [count ,setCount]= useState(0);
+const [count ,setCount]= useState(1);
     return (
         <div>
-       {description}
+             <th className='border border-slate-300'> {count}</th>
+             <th className='border border-slate-300'> {description}</th>
+             
+           
+      
         </div>
     );
 };
